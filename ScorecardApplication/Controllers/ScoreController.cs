79655e3d51b1b7f85204536ae900ae7afd7a940c
rossmcbain <ross.mcbain@bpomail.co.uk>
2017-06-29 17:01:17 +0100

    Added UI elements

---
+++ ScorecardApplication/Controllers/ScoreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScorecardApplication.Controllers
+{
+    public class ScoreController : Controller
+    {
+        // GET: Score
+        public ActionResult Score()
+        {
+            return View();
+        }
+    }
+}
